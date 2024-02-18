d29979c07e6fbb28f61ecc30bf0b473229891280
Enel Almonte <enel_almonte@ucne.edu.do>
2024-02-18 12:47:44 -0400

    Update build.gradle.kts with latest plugin versions

--- TicketCompose/build.gradle.kts
+++ TicketCompose/build.gradle.kts
@@ -1,9 +1,10 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 plugins {
-    id("com.android.application") version "8.1.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
-    id("com.google.devtools.ksp") version "1.9.10-1.0.13" apply false
+    id("com.android.application") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false //ultima version aqui: https://developer.android.com/jetpack/androidx/releases/compose-kotlin#kts
+    id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false //aqui se busca la ultima version https://github.com/google/ksp/releases
+    id("com.google.dagger.hilt.android") version "2.48" apply false
 }
 
 buildscript {
